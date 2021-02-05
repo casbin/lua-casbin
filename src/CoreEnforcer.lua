34b1257a7f7d14ae04e4ceba1a008f5cb746012b
turing0 <turing175@gmail.com>
2021-02-06 00:05:30 +0800

    add core files

--- src/CoreEnforcer.lua
+++ src/CoreEnforcer.lua
@@ -1,2 +1,22 @@
-CoreEnforcer = {}
+CoreEnforcer = {
+    model_path = 0,
+    length = 0,
+    breadth = 0,
+}
 
+function CoreEnforcer:new(o, length, breadth)
+    o = o or {}
+    setmetatable(o, self)
+    self.__index = self
+    self.length = length or 0
+    self.breadth = breadth or 0
+    self.area = length*breadth
+    return o
+end
+
+function CoreEnforcer:get()
+    return self.area;
+end
+
+
+return CoreEnforcer
