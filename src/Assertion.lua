acb280c84a713596065f98c3dc5b0002cf1d4765
Rushikesh Tote <rushi.tote@gmail.com>
2021-02-26 23:09:20 +0530

    Updated Assertion.lua
    Signed-off-by: Rushikesh Tote <rushi.tote@gmail.com>

---
+++ src/Assertion.lua
@@ -0,0 +1,19 @@
+--[[
+    * Assertion represents an expression in a section of the model.
+    * For example: r = sub, obj, act
+]]
+Assertion = {
+    key,
+    value,
+    tokens,
+    policy,
+    RM
+}
+
+function Assertion:buildRoleLinks(rm)
+    
+end
+
+function Assertion:buildIncrementalRoleLinks(rm, op, rules)
+    
+end
