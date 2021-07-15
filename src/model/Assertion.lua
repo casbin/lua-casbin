fc1f32d4aa8aec69aae693315a838d0fa0ccf1e0
Edmond <edomondja@gmail.com>
2021-07-16 00:31:42 +0800

    style: Adjust the format of the code and add things relate with policyMap
    Signed-off-by: Edmond <edomondja@gmail.com>

--- src/model/Assertion.lua
+++ src/model/Assertion.lua
@@ -27,7 +27,7 @@ function Assertion:new()
     o.tokens = {}
     o.policy = {}
     o.RM = {}
-    
+    o.policyMap={}
     setmetatable(o,self)
     self.__index = self
     return o
@@ -51,13 +51,13 @@ function Assertion:buildRoleLinks(rm)
         if #rule < count then
             error("grouping policy elements do not meet role definition")
         end
-        
+
         if rule[3] then
             self.RM:addLink(rule[1], rule[2], rule[3])
         else
             self.RM:addLink(rule[1], rule[2])
         end
-        
+
     end
 
 end
